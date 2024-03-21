f95c85129063c8b2fc8857f41ce32f9b7ef0fe2c
Helgi Rúnar Jóhannesson <helgirunar93@gmail.com>
2024-03-21 20:32:25 +0000

    Added meta tags for links

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './App.css';
 import Header from './pages/header';
 import AboutMe from './pages/aboutMe';
 import './App.css';
-import {Helmet} from 'react-helmet'
 
 function App() {
 	const [darkMode, setDarkMode] = useState(localStorage.getItem('darkmode') === 'true' ?? false);
@@ -14,11 +13,6 @@ function App() {
 	};
   return (
 	<div>
-		<Helmet>
-			<title>Helgi Rúnar Jóhannesson</title>
-			<meta name="description" content="Ferilskrá" />
-			<meta name="image" content={process.env.PUBLIC_URL + '/images/faceImage.jpg'} />
-		</Helmet>
 		<div className={darkMode ? 'dmm' : 'lmm'} style={{ minHeight: '100vh', height: '100%', width: '100%', display:'flex-col',justifyContent: 'center', flexFlow: 'row'}}>
 			<Header darkMode={darkMode} toggleDarkMode={toggleDarkMode}/>
 			<div className="centerContainer" style={{margin: 'auto'}} id="container">
