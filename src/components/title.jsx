52f95a61163bdb21fee8263ebfb3763bf91f4d3f
Helgi Rúnar Jóhannesson <helgirunar93@gmail.com>
2024-03-23 00:48:46 +0000

    Removed text wrap from titles

--- src/components/title.jsx
+++ src/components/title.jsx
@@ -4,7 +4,7 @@ const Title = ({ text, style, darkMode }) => {
   return (
     <div style={{...style, position: 'relative', display: 'flex' }}>
 		<div style={{backgroundColor: '#2465B4', height:'5px', width:'10%', flexGrow: '2', marginTop: 'auto', marginBottom: 'auto', marginRight: '4px', borderRadius: '25px'}}></div>
-		<div className="text-lg" style={{ paddingLeft: '4px', paddingRight: '4px', height:'100%', flexGrow: '1', width: 'auto', textAlign: 'center'}}>{text}</div>
+		<div className="text-lg" style={{ paddingLeft: '4px', paddingRight: '4px', height:'100%', flexGrow: '1', width: 'auto', textAlign: 'center', whiteSpace: 'nowrap'}}>{text}</div>
 		<div style={{backgroundColor: '#2465B4', marginLeft: '2px', height:'5px', width: '75%', marginTop: 'auto', marginBottom: 'auto', flexGrow: '2', marginLeft: '4px', borderRadius: '25px'}}></div>
     </div>
   );
