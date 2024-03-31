623f9a3a1dae9db75a424ac6f274b5f198c2fdf3
Helgi Rúnar Jóhannesson <helgirunar93@gmail.com>
2024-03-31 22:09:46 +0000

    Added new height for header

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 const Header = ({darkMode, toggleDarkMode}) => {
   return (
-		<div id="header" style={{backgroundColor: '#2465B4', height: '8vh', width: '100%'}}>
-			<div className="centerContainer" style={{height: '7.5vh', margin: 'auto', display: 'flex', position: 'relative'}}>
-				<div className="text-2xl" style={{color:'white', marginTop: 'auto', marginBottom: 'auto', marginLeft: '1vw'}}>Helgi Rúnar Jóhannesson</div>
+		<div id="header" className="headerHeight" style={{backgroundColor: '#2465B4', width: '100%'}}>
+			<div className="centerContainer headerHeight" style={{margin: 'auto', display: 'flex', position: 'relative'}}>
+				<div className="text-2xl" style={{color:'white', marginTop: 'auto', marginBottom: 'auto', marginLeft: '1vw', userSelect: 'none'}}>Helgi Rúnar Jóhannesson</div>
 				<div style={{position: 'absolute', right: '5px', display: 'flex', flexFlow: 'column', height:'100%'}}>
 					{/* <div style={{color: darkMode ? 'white' : 'white'}}>DarkMode</div> */}
 					<div onClick={toggleDarkMode} style={{height: '20px', width: '40px', borderRadius: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', margin: 'auto', backgroundColor: darkMode ? '#2c3e50' : '#ecf0f1', cursor: 'pointer', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)'}}>
