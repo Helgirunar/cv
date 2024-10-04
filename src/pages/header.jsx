652d954317646009c190132a253934476896f5fb
Helgi Rúnar Jóhannesson <helgirunar93@gmail.com>
2024-10-04 14:13:46 +0000

    Removed Um Mig og Verkefni links as they are not ready.

--- src/pages/header.jsx
+++ src/pages/header.jsx
@@ -34,10 +34,10 @@ const Header = ({darkMode, toggleDarkMode}) => {
 		<div id="header" className="headerHeight" style={{backgroundColor: '#2465B4', width: '100%', height: '7vh', zIndex: -1}}>
 			<div className="centerContainer headerHeight" style={{margin: 'auto', display: 'flex', position: 'relative', zIndex: 9, backgroundColor: '#2465B4'}}>
 				<div className="text-2xl" style={{color:'white', marginTop: 'auto', marginBottom: 'auto', marginLeft: '1vw', userSelect: 'none'}}>Helgi Rúnar Jóhannesson</div>
-					<div style={{color:'white', transition: 'ease-out 0.15s', marginLeft: 'auto', marginRight: '80px', marginTop: 'auto', marginBottom: 'auto',}}>
+					{/* <div style={{color:'white', transition: 'ease-out 0.15s', marginLeft: 'auto', marginRight: '80px', marginTop: 'auto', marginBottom: 'auto',}}>
 						<Link to="/" style={{color: "white", textDecoration: "none", marginRight: '10px'}}>Um mig</Link>
 						<Link to="/projects" style={{color: "white", textDecoration: "none"}}>Verkefni</Link>
-					</div>
+					</div> */}
 				<div style={{display: isMobile ? 'none' : 'block'}}>
 					<div style={{position: 'absolute', right: '5px', display: 'flex', flexFlow: 'column', height:'100%'}}>
 						<div onClick={toggleDarkMode} style={{height: '20px', width: '40px', borderRadius: '30px', display: 'flex', alignItems: 'center', justifyContent: 'center', position: 'relative', margin: 'auto', backgroundColor: darkMode ? '#2c3e50' : '#ecf0f1', cursor: 'pointer', boxShadow: '0px 0px 5px rgba(0, 0, 0, 0.2)'}}>
